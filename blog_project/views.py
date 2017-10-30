8df44e787aafb066c2cbe44ddf5f76203daa177d
xlyoung <13726251800@139.com>
2017-10-30 11:34:57 +0800

    Signed-off-by: xlyoung <13726251800@139.com>

---
+++ blog_project/views.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+    return render(request, 'index.html')
